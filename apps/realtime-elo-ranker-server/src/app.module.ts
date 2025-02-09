4663fa7a54ed5cbec96bde14e4cf22cee7a91d18
Irvyn <irvyncsm@gmail.com>
2025-02-09 20:39:42 +0100

    feat: implement match and ranking modules with services and controllers

--- apps/realtime-elo-ranker-server/src/app.module.ts
+++ apps/realtime-elo-ranker-server/src/app.module.ts
@@ -2,6 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MatchController } from './match/match.controller';
+import { PlayerController } from './player/player.controller';
+import { RankingController } from './ranking/ranking.controller';
+import { RankingService } from './ranking/ranking.service';
+import { RankingModule } from './ranking/ranking.module';
+import { MatchModule } from './match/match.module';
+import { PlayerModule } from './player/player.module';
 
 @Module({
   imports: [
@@ -11,8 +18,11 @@ import { AppService } from './app.service';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    PlayerModule,
+    RankingModule,
+    MatchModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PlayerController, MatchController, RankingController],
+  providers: [AppService, RankingService],
 })
 export class AppModule {}
--- apps/realtime-elo-ranker-server/src/player/dto/create-player.dto.ts
+++ apps/realtime-elo-ranker-server/src/player/dto/create-player.dto.ts
@@ -1,3 +1,4 @@
 export class CreatePlayerDto {
     id: string;
+    rank: number;
   }
